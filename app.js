6e966540202ee4a387e9303d6f3c9d099652e69c
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2021-10-09 22:41:18 -0300

    Melhorando o projeto e Tratando Errors

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 const routerProducts = require('./routes/products');
 const routerDemands = require('./routes/demands');
 
-app.use('/products',routerProducts);
-app.use('/demands',routerDemands);
+app.use(morgan('dev'));
+app.use('/products', routerProducts);
+app.use('/demands', routerDemands);
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagge: error.message
+        }
+    })
+});
 
 
 
